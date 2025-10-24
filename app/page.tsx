f72fa4b3fb8ac3539122fe6d36e86d6f9e39db5a
cheuyin <yinstanleycheung@gmail.com>
2025-10-24 15:29:44 -0700

    Create simple nav bar

--- app/page.tsx
+++ app/page.tsx
@@ -3,22 +3,30 @@ import Image from "next/image";
 export default function Home() {
     return (
         <div>
-            <header className="max-w-lg m-auto flex items-center justify-between">
-                <div>
-                    <h1 className="text-3xl font-bold">
-                        Hello, I&apos;m Stanley Cheung
-                    </h1>
-                    <h2 className="text-2xl">Freelance Web Developer</h2>
-                    <p>I try to build useful things on the web.</p>
-                </div>
-                <div className="relative w-25 h-25 rounded-full overflow-hidden">
-                    <Image
-                        src="/stanley_profile_picture.png"
-                        alt="My profile picture"
-                        fill
-                    />
-                </div>
+            <header className="p-5">
+                <p>Vancouver, Canada</p>
             </header>
+            <main className="max-w-2xl m-auto flex flex-col gap-10">
+                <header className=" flex items-center justify-between">
+                    <div>
+                        <h1 className="text-3xl font-bold">
+                            Hello, I&apos;m Stanley Cheung
+                        </h1>
+                        <h2 className="text-xl">Freelance Web Developer</h2>
+                        <p>I try to build useful things on the web.</p>
+                    </div>
+                    <div className="relative w-25 h-25 rounded-full overflow-hidden">
+                        <Image
+                            src="/stanley_profile_picture.png"
+                            alt="My profile picture"
+                            fill
+                        />
+                    </div>
+                </header>
+                <section>
+                    <h2 className="text-2xl">Connect With Me</h2>
+                </section>
+            </main>
         </div>
     );
 }
