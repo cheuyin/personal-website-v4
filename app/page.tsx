3bc2571718104f76e3e6e0c58f3b0c1b0f198d09
cheuyin <yinstanleycheung@gmail.com>
2025-10-24 15:14:29 -0700

    Add header section w/ profile picture

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,24 @@
+import Image from "next/image";
+
 export default function Home() {
     return (
         <div>
-            <h1>Hello, I&apos;m Stanley Cheung</h1>
-            <h2>Freelance Web Developer</h2>
-            <p>Welcome to my site!</p>
-            <p>
-                I use my deep knowledge in Next.js to build fast, secure, and
-                useful websites.
-            </p>
+            <header className="max-w-lg m-auto flex items-center justify-between">
+                <div>
+                    <h1 className="text-3xl font-bold">
+                        Hello, I&apos;m Stanley Cheung
+                    </h1>
+                    <h2 className="text-2xl">Freelance Web Developer</h2>
+                    <p>I try to build useful things on the web.</p>
+                </div>
+                <div className="relative w-25 h-25 rounded-full overflow-hidden">
+                    <Image
+                        src="/stanley_profile_picture.png"
+                        alt="My profile picture"
+                        fill
+                    />
+                </div>
+            </header>
         </div>
     );
 }
